a5dfc0d654eff572285545a54550cb4c64bb592d
Lucas Cardoso <lucascardoso910@gmail.com>
2024-05-24 23:14:50 +0200

    clean files unrelated to final game

--- src/main.cpp
+++ src/main.cpp
@@ -2,59 +2,64 @@
 
 #include <iostream>
 
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+
+const sf::Color backgroundColor = sf::Color::Black;
+
+void createWindow(sf::RenderWindow &window)
+{
+    sf::VideoMode videoMode(800, 600);
+    
+    sf::Uint32 styles = sf::Style::Default;
+
+    std::vector<sf::VideoMode> fullscreenModes= sf::VideoMode::getFullscreenModes();
+    if (fullscreenModes.size() > 0)
+    {
+        videoMode = fullscreenModes[0];
+        // styles = sf::Style::Fullscreen;
+    }
+
+    window.create(videoMode, "Dawn of Empires", styles);
+    window.setFramerateLimit(60);
+}
+
+void handleInput(sf::RenderWindow &window)
+{
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        switch (event.type)
+        {
+        case sf::Event::EventType::Closed:
+            window.close();
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
+void updateFrame()
+{
+}
  
+void renderFrame(sf::RenderWindow &window)
+{
+    window.clear(backgroundColor);
+    window.display();
+}
+
 int main()
 {
-    std::cout << "Build path is " BASE_PATH << std::endl;
+    sf::RenderWindow window;
+    createWindow(window);
 
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
- 
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(BASE_PATH "/resources/cute_image.jpg"))
-        return EXIT_FAILURE;
-    sf::Sprite sprite(texture);
- 
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(BASE_PATH "/resources/arial.ttf"))
-        return EXIT_FAILURE;
-    sf::Text text("Hello SFML", font, 50);
- 
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(BASE_PATH "/resources/nice_music.mp3"))
-        return EXIT_FAILURE;
- 
-    // Play the music
-    music.play();
- 
-    // Start the game loop
     while (window.isOpen())
     {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
- 
-        // Clear screen
-        window.clear();
- 
-        // Draw the sprite
-        window.draw(sprite);
- 
-        // Draw the string
-        window.draw(text);
- 
-        // Update the window
-        window.display();
+        handleInput(window);
+        updateFrame();
+        renderFrame(window);
     }
  
     return EXIT_SUCCESS;
